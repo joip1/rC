65771a19772055843d1feb85fbac19a006ff7aee
Rodrigo Cabaço <rodrigonunescabaco@gmail.com>
2020-08-14 20:07:38 +0100

    Initial and Very Early Release!
    Open Source Programming Language Created By Rodrigo Cabaço =) This is a free and totaly open source compiler with rC built-in. Feel free to use it!
    
    < Rodrigo Cabaço, rC >

---
+++ Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace rC
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string readline;
+            List<string> codeLines = new List<string>();
+            string[] varTypes = new string[] { "number", "str", "save(this)", "Write", "WriteStr", "WriteNum"};
+            string[] methods = new string[] { "Write", "WriteStr", "WriteNum" };
+
+
+            Console.Write("0 ");
+
+
+            while ((readline = Console.ReadLine()).ToLower() != "rcompiler.compile")
+            {
+
+                
+                if (varTypes.Any(readline.StartsWith))
+                {
+                    Console.Clear();
+                    foreach (var line in codeLines)
+                    {
+                        if(line == "save(this)")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(codeLines.IndexOf(line) + " " + line + "\n");
+                            Console.ResetColor();
+                        }
+                        if (methods.Any(line.StartsWith) && line != "save(this)")
+                        {
+                            Console.ResetColor();
+                            Console.Write(codeLines.IndexOf(line) + " ");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(line.Split('&').First());
+                            Console.ResetColor();
+                            try
+                            {
+                                Console.Write("&>" + line.Split(new[] { "&>" }, StringSplitOptions.None).Last() + "\n");
+                            }
+                            catch
+                            {
+                                Console.Write(" <---- Invalid Syntax\n");
+                            }
+                        }
+                        if (varTypes.Any(line.StartsWith) && methods.Any(line.StartsWith) == false && line != "save(this)")
+                        {
+                            Console.ResetColor();
+                            Console.Write(codeLines.IndexOf(line) + " ");
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.Write(line.Split(' ').First());
+                            Console.ForegroundColor = ConsoleColor.White;
+                            try
+                            {
+                                Console.Write(" " + line.Split(' ')[1] + " >>" + line.Split('>').Last() + "\n");
+                            }
+                            catch
+                            {
+                                Console.Write(" <---- Invalid Syntax\n");
+                            }
+                        }
+                       
+                    }
+                    if(methods.Any(readline.StartsWith) == false && readline != "save(this)")
+                    {
+                        Console.ResetColor();
+                        Console.Write(codeLines.Count + " ");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.Write(readline.Split(' ').First());
+                        Console.ForegroundColor = ConsoleColor.White;
+                        try
+                        {
+                            Console.Write(" " + readline.Split(' ')[1] + " >>" + readline.Split('>').Last() + "\n");
+                        }
+                        catch
+                        {
+                            Console.Write(" <---- Invalid Syntax\n");
+                        }
+
+
+                    } else if (readline == "save(this)")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(codeLines.Count + " " + readline + "\n");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+
+                        Console.ResetColor();
+                        Console.Write(codeLines.Count + " ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(readline.Split('&').First());
+                        Console.ResetColor();
+                        try
+                        {
+                            Console.Write("&>" + readline.Split(new[] { "&>" }, StringSplitOptions.None).Last() + "\n");
+                        }
+                        catch
+                        {
+                            Console.Write(" <---- Invalid Syntax\n");
+                        }
+                    }
+
+
+
+
+                }
+                
+                if (readline.StartsWith("line.modify") && readline.Contains(":"))
+                {
+                    int modifyLineIndex = Convert.ToInt32(readline.Split(new[] { "line.modify(" }, StringSplitOptions.None).Last().Split(')').First());
+                    try
+                    {
+                        codeLines[modifyLineIndex] = readline.Split(new[] { ": " }, StringSplitOptions.None).Last();
+                    }
+                    catch
+                    {
+                        Console.Write(" <----- Invalid Syntax (Line not Found)\n");
+                    }
+                    Console.Clear();
+                    foreach (var modified in codeLines)
+                    {
+                        if (varTypes.Any(modified.StartsWith))
+                        {
+                            if (modified == "save(this)")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.Write(codeLines.IndexOf(modified) + " " + modified + "\n");
+                                Console.ResetColor();
+                            }
+                            if (methods.Any(modified.StartsWith) == false && modified != "save(this)")
+                            {
+
+
+                                Console.ResetColor();
+                                Console.Write(codeLines.IndexOf(modified) + " ");
+                                Console.ForegroundColor = ConsoleColor.Magenta;
+                                Console.Write(modified.Split(' ').First());
+                                Console.ForegroundColor = ConsoleColor.White;
+                                try
+                                {
+                                    Console.Write(" " + modified.Split(' ')[1] + " >>" + modified.Split('>').Last() + "\n");
+                                }
+                                catch
+                                {
+                                    Console.Write(" <---- Invalid Syntax\n");
+                                }
+                            }
+                            else
+                            {
+
+                                Console.ResetColor();
+                                Console.Write(codeLines.IndexOf(modified) + " ");
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write(modified.Split('&').First());
+                                Console.ResetColor();
+                                try
+                                {
+                                    Console.Write("&>" + modified.Split(new[] { "&>" }, StringSplitOptions.None).Last() + "\n");
+                                }
+                                catch
+                                {
+                                    Console.Write(" <---- Invalid Syntax\n");
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    codeLines.Add(readline);
+                    Console.Write("" + codeLines.Count + " ");
+                }
+            }
+            rCompiler.Compile(codeLines);
+            Console.WriteLine("\n------------------------------------------------------\nCompiled, Output is Above\nPress enter to exit...");
+            Console.ReadLine();
+        }
+        public static void WriteId(int id)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n\n------------------------------------------------------\n ----> Code Saved With Id " + id + " <---- \n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ rCompiler.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace rC
+{
+    public class rCompiler
+    {
+        public static void Compile(List<string> code)
+        {
+            Console.Clear();
+            //values and indicators
+            List<string> numberNames = new List<string>();
+            List<double> numberValues = new List<double>();
+            List<string> strNames = new List<string>();
+            List<string> strValues = new List<string>();
+            int id = 0;
+            //read code line by line
+            foreach (var line in code)
+            {
+                if (line.StartsWith("number") && line.Contains(">>"))
+                {
+                    try
+                    {
+                        if(numberNames.Contains(line.Split(' ')[1].Split('>').First()))
+                        {
+                            numberValues[numberNames.IndexOf(line.Split(' ')[1].Split('>').First())] = Convert.ToDouble(line.Split('>').Last().Split(' ').Last());
+                        }
+                        else
+                        {
+                            numberNames.Add(line.Split(' ')[1].Split('>').First());
+                            numberValues.Add(Convert.ToDouble(line.Split('>').Last().Split(' ').Last()));
+                        }
+                    }
+                    catch
+                    {
+                        int errorLine = code.IndexOf(line);
+                        Console.WriteLine($"Invalid Syntax (Line {errorLine--})");
+                    }
+                }
+
+                if (line.StartsWith("str") && line.Contains(">>"))
+                {
+                    try
+                    {
+                        if (strNames.Contains(line.Split(' ')[1].Split('>').First()))
+                        {
+                            strValues[strNames.IndexOf(line.Split(' ')[1].Split('>').First())] = line.Split('>').Last();
+                        }
+                        else
+                        {
+                            strNames.Add(line.Split(' ')[1].Split('>').First());
+                            strValues.Add(line.Split('>').Last());
+                        }
+                       
+                    }
+
+                    catch
+                    {
+                        int errorLine = code.IndexOf(line);
+                        Console.WriteLine($"Invalid Syntax (Line {errorLine--})");
+                    }
+                }
+
+                if (line.StartsWith("list(str)"))
+                {
+                    List<List<string>> listsStr = new List<List<string>>(); 
+                }
+
+
+
+
+
+
+                //Write 
+
+                if (line.ToLower() == "newline" || line.ToLower() == "newln")
+                {
+                    Console.WriteLine("");
+                }
+                if (line.StartsWith("Write") && line.Contains("&>") && line.Contains("<&") && line.Contains("WriteStr") == false && line.Contains("WriteNum") == false)
+                {
+                    //check if it is a number 
+                    var matchesNumber = numberNames.Where(x => line.Contains(line.Split(new[] { "Write &>" }, StringSplitOptions.None).Last().ToString().Split(new[] { "<&" }, StringSplitOptions.None).First().ToString()));
+
+                    Console.Write(line.Split(new[] { "Write &>" }, StringSplitOptions.None).Last().ToString().Split(new[] { "<&" }, StringSplitOptions.None).First().ToString() + " ");
+
+                }
+                else if (line.StartsWith("WriteStr") && line.Contains("&>"))
+                {
+                    foreach (var name in strNames)
+                    {
+                        try
+                        {
+                            if (line.Contains(name))
+                            {
+                                Console.Write(strValues[strNames.IndexOf(name)] + line.Split(new[] { name}, StringSplitOptions.None)[1].Split(new[] { "<&" }, StringSplitOptions.None).First());
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Invalid Syntax on Line " + code.IndexOf(line));
+                        }
+                    }
+
+                }
+                else if (line.StartsWith("WriteNum") && line.Contains("&>"))
+                {
+                    foreach (var name in numberNames)
+                    {
+                        if (line.Contains(name))
+                        {
+                            try
+                            {
+                                Console.Write(numberValues[numberNames.IndexOf(name)] + line.Split(new[] { name }, StringSplitOptions.None)[1].Split(new[] { "<&"}, StringSplitOptions.None).First());
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Invalid Syntax on Line " + code.IndexOf(line));
+                            }
+                        }
+                    }
+
+
+
+
+
+                    //save
+                    if (line == "save(this)")
+                    {
+                        id = new Random().Next(1, 10000);
+                        StreamWriter writer = File.CreateText("code.id(" + id + ").txt");
+                        foreach (var lineToSave in code)
+                        {
+                            writer.WriteLine(lineToSave);
+                        }
+                        writer.Close();
+                    }
+                    else if (line == "save = false")
+                    {
+
+                    }
+                }
+
+                if (id != 0)
+                {
+                    Program.WriteId(id);
+                }
+
+
+                //for
+            }
+        }
+    }
+}
