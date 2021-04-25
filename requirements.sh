ee0561d7edd1771c2b3ba7c8a0fc8c2983ecbd05
Rodrigo Cabaço <42916568+RodrigoCabaco@users.noreply.github.com>
2021-04-25 19:13:36 +0100

    Add requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+sudo apt install gnupg ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install mono
+sudo apt install mono-complete
+./install_linux.sh
+wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-5.0
